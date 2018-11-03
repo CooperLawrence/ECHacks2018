04f5750bef3539a5d01bad5a5036473ec93e5c2e
Robert Conrad <rconr060@uottawa.ca>
2018-11-03 07:09:58 -0400

    cleaned string names, make navigation basis

--- app/src/main/java/echacks2018/lumberlog/MainActivity.java
+++ app/src/main/java/echacks2018/lumberlog/MainActivity.java
@@ -100,16 +100,16 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.tree_identifier) {
             fragmentClass = TreeListFragment.class ;
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.tree_list) {
             fragmentClass = LocateLoggingFragment.class ;
-        } else if (id == R.id.nav_slideshow) {
+        }/* else if (id == R.id.nav_slideshow) {
             fragmentClass = TreeListFragment.class ;
         } else if (id == R.id.nav_manage) {
             fragmentClass = TreeListFragment.class ;
-        } else if (id == R.id.nav_share) {
+        }*/ else if (id == R.id.nav_share) {
             fragmentClass = TreeListFragment.class ;
         } else if (id == R.id.nav_send) {
             fragmentClass = TreeListFragment.class ;
